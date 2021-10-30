ecba1c19574fe471c94dd1ff4087ef0fda08af75
Guillaume De Martino <guillaumedemartino@gmail.com>
2021-10-30 19:48:11 +0200

    [mediaGrid] Tap to close instead of button

--- src/components/MediaGrid.js
+++ src/components/MediaGrid.js
@@ -68,7 +68,7 @@ export default function MediaGrid(props) {
           }}
           sx={{ zIndex: 1001 }}
         >
-          <div>
+          {/* <div>
             <button
               className="twetch-renderer__media-grid-close"
               onClick={(e) => {
@@ -82,10 +82,10 @@ export default function MediaGrid(props) {
                 <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
               </svg>
             </button>
-          </div>
+          </div> */}
           <ImageSlides
             index={images.findIndex((url) => url === imageSlide)}
-            tapClose={false}
+            tapClose={true}
             images={images}
             isOpen={open}
             showPageButton={true}
